2a7c57d8fd6c927c3288508904551c649f972b18
Squeak-Paradox <olliemchurch@gmail.com>
2021-09-13 18:24:42 +0100

    Update OptionsMenu.js

--- src/components/OptionsMenu.js
+++ src/components/OptionsMenu.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext} from 'react'
 import '../css/components/OptionsMenu.css'
 
 import {BackgroundMusicContext} from '../context/BackgroundMusicContext'
